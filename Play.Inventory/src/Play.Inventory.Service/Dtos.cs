d2eaa151cfbc2e41fb8c8d2436b0003552bade69
Fabio Puissant <fabio.puissant@hotmail.com>
2022-08-19 19:43:55 +0200

    Play.Inventory.Service project add

---
+++ Play.Inventory/src/Play.Inventory.Service/Dtos.cs
@@ -0,0 +1,8 @@
+﻿using System;
+namespace Play.Inventory.Service
+{
+	public record GrantItemDto(Guid UserId, Guid CatalogItemId, int Quantity);
+
+	public record InventoryItemDto(Guid CatalogItemId, int Quality, DateTimeOffset AcquiredDate);
+}
+
