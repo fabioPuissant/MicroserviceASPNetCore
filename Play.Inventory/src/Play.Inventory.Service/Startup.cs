44b4a88b7824404423c3904ee3592874dd116ede
Fabio Puissant <fabio.puissant@hotmail.com>
2022-08-22 21:16:17 +0200

    Added exponential back-off policy for failing Microservice to prevent overwhelming a microservice

--- Play.Inventory/src/Play.Inventory.Service/Startup.cs
+++ Play.Inventory/src/Play.Inventory.Service/Startup.cs
@@ -8,6 +8,8 @@ using Play.Common.MongoDB;
 using Play.Common.Settings;
 using Play.Inventory.Service.Clients;
 using Play.Inventory.Service.Entities;
+using Polly;
+using Polly.Timeout;
 
 namespace Play.Catalog.Service
 {
@@ -31,9 +33,24 @@ namespace Play.Catalog.Service
             // Repositories registration with the extension method
             services.AddMongoRepository<InventoryItem>("inventoryitems");
 
+            Random jitterer = new Random(); // random wait
+
             services.AddHttpClient<CatalogClient>(client => {
                 client.BaseAddress = new Uri("https://localhost:5001");
-            });
+            })
+                .AddTransientHttpErrorPolicy(builder => builder.Or<TimeoutRejectedException>().WaitAndRetryAsync( // prevents keep sending request (overwhelming) by backing-off policy
+                    5, // number of attempts
+                    retryAttampt => TimeSpan.FromSeconds(Math.Pow(2, retryAttampt))
+                                    + TimeSpan.FromMilliseconds(jitterer.Next(0, 1000)), // exponential back-off with randomness,
+                    onRetry: (outcome, timespan, retryAttempt) =>
+                    {
+                        //remove this(onRetry) in production
+                        var serviceProvider = services.BuildServiceProvider();
+                        serviceProvider.GetService<ILogger<CatalogClient>>()
+                            .LogWarning($"Delaying for {timespan.TotalSeconds} seconds, then making retry {retryAttempt}");
+                    }
+                ))
+                .AddPolicyHandler(Policy.TimeoutAsync<HttpResponseMessage>(1));
 
             services.AddControllers(options =>
             {
