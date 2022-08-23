13b724e1295ba373629e3e60bf34462be00ad306
Fabio Puissant <fabio.puissant@hotmail.com>
2022-08-24 00:13:44 +0200

    fixed changes

---
+++ Play.Inventory/src/Play.Inventory.Service/Entities/CatalogItem.cs
@@ -0,0 +1,17 @@
+﻿using Play.Common;
+
+namespace Play.Inventory.Service.Entities
+{
+	public class InventoryItem : IEntity
+	{
+        public Guid UserId { get; set; }
+
+        public Guid CatalogItemId { get; set; }
+
+        public int Quantity { get; set; }
+
+        public DateTimeOffset AcquiredDate { get; set; }
+        public Guid Id { get; set; }
+    }
+}
+
